7b57d4788bcc9212cfaf63b94e84a934afc42cdb
Alex Pavlov <halfer.orc@gmail.com>
2021-02-10 06:08:04 +0300

    fix(footer) semantic and mobile

--- src/components/NavFooter/NavFooter.js
+++ src/components/NavFooter/NavFooter.js
@@ -8,34 +8,40 @@ export default function NavFooter({isSimple}) {
 
 
   return (
-    <div className={`nav-footer${isSimple ? ' nav-footer_simple' : ''}`}>
-      <a 
-        href='tel: +79254277254'
-        rel='noopener noreferrer'
-        target='_blank'
-      >
-        <PhoneIcon 
-          style={ isSimple ? { color: 'white' } : { color: 'orange' } }
-        />
-      </a>
-      <a 
-        href='mailto: dotheimpossible@protonmail.com'
-        rel='noopener noreferrer'
-        target='_blank'
-      >
-        <EmailIcon 
-          style={ isSimple ? { color: 'white' } : { color: 'orange' } }
-        />
-      </a>
-      <a 
-        href='https://github.com/Sh4n-Oldone'
-        rel='noopener noreferrer'
-        target='_blank'
-      >
-        <GitHubIcon 
-          style={ isSimple ? { color: 'white' } : { color: 'orange' } }
-        />
-      </a>
-    </div>
+    <ul className={`nav-footer${isSimple ? ' nav-footer_simple' : ''}`}>
+      <li>
+        <a 
+          href='tel: +79254277254'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <PhoneIcon 
+            style={ isSimple ? { color: 'white' } : { color: 'orange' } }
+          />
+        </a>
+      </li>
+      <li>
+        <a 
+          href='mailto: dotheimpossible@protonmail.com'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <EmailIcon 
+            style={ isSimple ? { color: 'white' } : { color: 'orange' } }
+          />
+        </a>
+      </li>
+      <li>
+        <a 
+          href='https://github.com/Sh4n-Oldone'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <GitHubIcon 
+            style={ isSimple ? { color: 'white' } : { color: 'orange' } }
+          />
+        </a>
+      </li>
+    </ul>
   );
 }
